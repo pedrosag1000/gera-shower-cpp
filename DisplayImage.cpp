4c14be869537e50f98c4a047f3687b4259633acd
Pedro Sag <pedrosag1000@gmail.com>
2023-05-27 00:25:17 -0700

    new version with read from uart and show some data from input

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,13 +1,30 @@
 #include <opencv2/opencv.hpp>
 #include <math.h>
+#include <CppLinuxSerial/SerialPort.hpp>
+#include <string.h>
 
 using namespace cv;
 
 using namespace std;
 
+using namespace mn::CppLinuxSerial;
+
 #define PI 3.14159265
 
-void draw_arch(Mat frame, Point center, int circle_radius, int view_angle, int size_of_angle, Scalar color, int thickness)
+void draw_text_center(Mat frame, string text, Point center, int fontFace, double fontScale, Scalar color, int thickness)
+{
+    int baseLine = 0;
+    Size textSize = getTextSize(text, fontFace, fontScale, thickness, &baseLine);
+    baseLine += thickness;
+
+    Point textOrg = Point(center.x - textSize.width / 2, center.y + textSize.height / 2);
+    // rectangle(frame, textOrg + Point(0, baseLine), textOrg + Point(textSize.width, -textSize.height), color);
+    // line(frame, textOrg + Point(0, thickness), textOrg + Point(textSize.width, thickness), color);
+
+    putText(frame, text, textOrg, fontFace, fontScale, color, thickness);
+}
+
+void draw_arch(Mat frame, Point center, int circle_radius, int view_angle, int size_of_angle, Scalar color, int thickness, bool drawAngle = false,double fontSize=0.5,bool overrideAngleText=false,int overrideAngle=0)
 {
 
     int half_of_size_of_angle = size_of_angle / 2;
@@ -32,23 +49,67 @@ void draw_arch(Mat frame, Point center, int circle_radius, int view_angle, int s
     line(
         frame,
         center,
-        Point(center.x + diff_x, center.y + diff_y),
+        center + Point(diff_x, diff_y),
         color, thickness);
 
+    if (drawAngle)
+    {
+        diff_x = int(cos((view_angle) / 180.0 * PI) * circle_radius);
+        diff_y = int(sin((view_angle) / 180.0 * PI) * circle_radius);
 
+        draw_text_center(frame, to_string(overrideAngle), center + Point(diff_x * 0.8, diff_y * 0.8), FONT_HERSHEY_SIMPLEX, fontSize, color, thickness);
+    }
+}
 
-    
+vector<string> explode(const string &s, const char &c)
+{
+    string buff{""};
+    vector<string> v;
+
+    for (auto n : s)
+    {
+        if (n != c)
+            buff += n;
+        else if (n == c && buff != "")
+        {
+            v.push_back(buff);
+            buff = "";
+        }
+    }
+    if (buff != "")
+        v.push_back(buff);
+
+    return v;
 }
 
 int main()
 {
 
     Mat frame;
-    int frame_id=0;
+    int frame_id = 0;
 
     namedWindow("Display window");
 
     VideoCapture cap("/dev/video1");
+
+    // Create serial port object and open serial port at 57600 buad, 8 data bits, no parity bit, one stop bit (8n1),
+    // and no flow control
+    SerialPort serialPort("/dev/ttyTHS2", BaudRate::B_115200, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
+    // Use SerialPort serialPort("/dev/ttyACM0", 13000); instead if you want to provide a custom baud rate
+    serialPort.SetTimeout(1); // Block for up to 0ms to receive data
+    serialPort.Open();
+
+    // WARNING: If using the Arduino Uno or similar, you may want to delay here, as opening the serial port causes
+    // the micro to reset!
+
+    // Read some data back (will block for up to 100ms due to the SetTimeout(100) call above)
+    string readData, allReadData, startString = "Start:", endString = ":End";
+    bool isDataStarted = false;
+    int startPosition = -1, endPosition = -1;
+
+    int radar_angle = -100, radar_size_of_angle = 20;
+    int view_angle = -100, view_size_of_angle = 20;
+    int horizental_angle = 60, vertical_angle = 60, zoom = 1;
 
     cap.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
 
@@ -57,14 +118,13 @@ int main()
 
     if (!cap.isOpened())
     {
-
         cout << "cannot open camera";
     }
 
     int pressed_key = 10;
     while (pressed_key != 27)
     {
-        frame_id= frame_id + 1 % 1000;
+        frame_id = frame_id + 1 % 1000;
         cap >> frame;
 
         int width = frame.cols;
@@ -104,64 +164,126 @@ int main()
                 1);
         }
 
-        // Draw circle on right top
+        // Draw radar circle on right top
         int circle_radius = int(quarter_height / 2);
-        Point circle_center = Point(int(width - circle_radius) - 10,circle_radius + 10);
-        
+        Point circle_center = Point(int(width - circle_radius) - 10, circle_radius + 10);
+
         circle(frame, circle_center, circle_radius,
                Scalar(0, 255, 0));
 
-        int view_angle = 180 + frame_id;
-        int size_of_angle = 30;
-
         draw_arch(
             frame,
             circle_center,
             circle_radius,
-            view_angle,
-            size_of_angle,
-            Scalar(255, 255, 0),
-            3);
+            // -90 is for start angle from top of the circle not the right angle of it
+            (radar_angle < 0 ? (180 + frame_id) : radar_angle) - 90,
+            radar_size_of_angle,
+            Scalar(0, 0, 255),
+            2,true,0.4,true,radar_angle);
 
+        // putText(frame,
+        //         to_string(radar_angle < 0 ? 180 + frame_id : radar_angle),
 
+        //         circle_center, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
 
         // Draw view sight
         circle_radius = int(quarter_height);
-        circle_center = Point(int(width - circle_radius ) - 10,(quarter_height + 10) * 2);
+        circle_center = Point(int(width - circle_radius) - 10, height - circle_radius * .5);
 
-
-        view_angle = 360 - 45;
-        size_of_angle = 90;
         draw_arch(
             frame,
             circle_center,
             circle_radius,
-            view_angle,
-            size_of_angle,
+            (270 + 370) / 2,
+            (370 - 270) + view_size_of_angle,
             Scalar(0, 255, 0),
-            1
-        );
-        view_angle = (280 + frame_id % 70);
-        size_of_angle = 20;
+            1);
+
+        int view_angle_temp = view_angle < -90 ? (frame_id / 10 % 100) - 10 : view_angle;
+        view_angle_temp = ((view_angle_temp + 10) % 100) - 10;
+
         draw_arch(
             frame,
             circle_center,
             circle_radius,
-            view_angle,
-            size_of_angle,
-            Scalar(255, 255, 0),
-            3
-        );
+            10 - (view_angle_temp + 10),
+            view_size_of_angle,
+            Scalar(0, 0, 255),
+            2,true,.7,true,view_angle_temp);
 
+        // putText(frame,
+        //         to_string(view_angle_temp),
 
+        //         circle_center, FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
 
+        // show horizental angle
 
+        draw_text_center(frame, to_string(horizental_angle / 2), Point(width - line_width, half_height), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 0, 255), 2);
+        draw_text_center(frame, to_string(-horizental_angle / 2), Point(line_width, half_height), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 0, 255), 2);
+
+        // show vertical angle
+        draw_text_center(frame, to_string(vertical_angle / 2), Point(half_width, line_height), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 0, 255), 2);
+        draw_text_center(frame, to_string(-vertical_angle / 2), Point(half_width, height - line_height), FONT_HERSHEY_SIMPLEX, .7, Scalar(0, 0, 255), 2);
+
+        // show zoom
+        draw_text_center(frame, to_string(zoom) + "X", Point(line_width, line_height), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
 
         imshow("Display window", frame);
 
         pressed_key = waitKey(25);
+        readData.clear();
+        serialPort.Read(readData);
+
+        allReadData.append(readData);
+
+        if (!isDataStarted)
+        {
+            startPosition = allReadData.rfind(startString);
+            if (startPosition >= 0)
+            {
+                allReadData = allReadData.substr(startPosition + startString.length(), allReadData.length());
+                isDataStarted = true;
+                startPosition = -1;
+            }
+        }
+        else
+        {
+            endPosition = allReadData.rfind(endString);
+            if (endPosition >= 0)
+            {
+                allReadData = allReadData.substr(0, endPosition);
+
+                // process data in allReadData
+                vector<string> output = explode(allReadData, ',');
+
+                if (output.size() == 5)
+                {
+                    cout << "Processed data: " << allReadData << endl;
+                    radar_angle = stoi(output.at(0));
+                    view_angle = stoi(output.at(1));
+                    vertical_angle = stoi(output.at(2));
+                    horizental_angle = stoi(output.at(3));
+                    zoom = stoi(output.at(4));
+                }
+                else
+                {
+                    cout << "Data size is correct" << endl;
+                }
+
+                isDataStarted = false;
+                endPosition = -1;
+                // flush
+                allReadData.clear();
+            }
+        }
+
+        if (!readData.empty())
+        {
+            cout << "Recieved data = \"" << readData << "\"" << endl;
+        }
     }
 
+    // Close the serial port
+    serialPort.Close();
     return 0;
 }
-
